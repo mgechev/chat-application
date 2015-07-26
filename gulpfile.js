168d30e3628b250eb8059cb9789ec4154c7637d1
nshahpazov <nshahpazov@gmail.com>
2015-07-26 20:08:49 +0300

    added a better comment to what the script has to execute

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var typescript = require('typescript');
 
+// make a task which runs this script
+//  node node_modules/typescript/bin/tsc.js app/src/* --jsx 'react' --module commonjs
+
+
 gulp.task('default', function () {
   var tsResult = gulp.src('./app/src/')
     .pipe(ts({
