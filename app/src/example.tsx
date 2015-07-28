3211c8e7f41f581da9e42caa9c320b086c43a177
nshahpazov <nshahpazov@gmail.com>
2015-07-28 15:26:12 +0300

    example usage of dispatcher

--- app/src/example.tsx
+++ app/src/example.tsx
@@ -1,18 +1,27 @@
 /// <reference path="../../typing/react.d.ts" />
 
 import React = require('react');
+import Dispatcher = require('./dispatcher/Dispatcher');
 
 class Example extends React.Component<any, any> {
-    private foo: number;
-    constructor() {
-        super();
-        this.foo = 42;
-    }
-    render() {
-        return (
-            <div>This is an example class!</div>
+  private foo: number;
+
+  constructor() {
+    super();
+    this.foo = 42;
+  }
+
+  render() {
+    /// testing
+    let d = new Dispatcher();
+    d.register(() => {
+      console.log('lorem dispatched');
+    });
+
+    return (
+      <div>This is an example class!</div>
     );
-    }
+  }
 }
 
 export = Example;
